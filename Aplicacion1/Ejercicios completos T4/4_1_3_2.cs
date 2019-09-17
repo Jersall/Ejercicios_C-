c82097bf404478d682168bfd5d09bb387cb51b57
Juanjo <JRafet20@gmail.com>
2019-09-17 10:46:54 +0200

    4.1.3.3 completado y añadido

--- Aplicacion1/Ejercicios completos T4/4_1_3_2.cs
+++ Aplicacion1/Ejercicios completos T4/4_1_3_2.cs
@@ -1,6 +1,7 @@
 ﻿/*Ejercicio 4.1.3.2: Crea un programa que pregunte al usuario cuántos números 
 enteros va a introducir(por ejemplo, 10), le pida todos esos números, 
-los guarde en un array y finalmente calcule y muestre la media de esos números.*/using System;
+los guarde en un array y finalmente calcule y muestre la media de esos números.*/
+using System;
 public class E4_1_3_2
 {
     public static void Ejecutar()
---
+++ Aplicacion1/Ejercicios completos T4/4_1_3_3.cs
@@ -0,0 +1,28 @@
+﻿/*Ejercicio 4.1.3.3: Un programa que pida al usuario 10 reales de doble precisión, calcule su
+media y luego muestre los que están por encima de la media.*/
+using System;
+public class E4_1_3_3
+{
+    public static void Ejecutar()
+    {
+        double[] dieznums = new double[10];
+        double mednums, suma = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("¿Número {0}?: ", i + 1);
+            dieznums[i] = Convert.ToDouble(Console.ReadLine());
+            suma += dieznums[i];
+        }
+        mednums = suma / 10;
+        Console.WriteLine("La media es {0}", mednums);
+        for (int i = 0; i < 10; i++)
+        {
+            if (dieznums[i] > mednums)
+            {
+                Console.Write("({0}), ", dieznums[i]);
+            }
+        }
+        Console.WriteLine("Están por encima de la media.");
+    }
+}
+
