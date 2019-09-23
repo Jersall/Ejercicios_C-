00551e110880c2367b2dafc0a4c4029fbce2f4ba
Juanjo <JRafet20@gmail.com>
2019-09-23 12:48:14 +0200

    Completado 4.1.3.8

---
+++ Aplicacion1/Ejercicios completos T4/4_1_3_8.cs
@@ -0,0 +1,22 @@
+﻿/*Ejercicio 4.1.3.8: Un programa que reserve espacio para un vector de 3 componentes, pida
+al usuario valores para dichas componentes (por ejemplo [2, -5, 7]) y muestre su
+módulo (raíz cuadrada de la suma de sus componentes al cuadrado).*/
+using System;
+public class E4_1_3_8
+{
+    public static void Ejecutar()
+    {
+        double[] numeros = new double[3];
+        double[] cuadrados = new double[3];
+        double suma = 0, raiz;
+        for (int i = 0; i <= 2; i++)
+        {
+            Console.Write("Introduce un número({0}): ", i + 1);
+            numeros[i] = Convert.ToDouble(Console.ReadLine());
+            cuadrados[i] = numeros[i] * numeros[i];
+            suma += cuadrados[i];
+        }
+        raiz = Math.Sqrt(suma);
+        Console.WriteLine("El módulo de los 3 números es {0}", raiz);
+    }
+}
