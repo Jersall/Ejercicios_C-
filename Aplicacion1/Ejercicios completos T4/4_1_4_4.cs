5155f7ab819bce55d9da6bb846b236e22ae89f68
Juanjo <JRafet20@gmail.com>
2019-10-01 11:55:36 +0200

    Pequeño improve 4_1_4_4

--- Aplicacion1/Ejercicios completos T4/4_1_4_4.cs
+++ Aplicacion1/Ejercicios completos T4/4_1_4_4.cs
@@ -7,7 +7,7 @@ public class E4_1_4_4
     public static void Ejecutar()
     {
         int[] numeros = { 10, 15, 12, 11, 13 };
-        int cant = 5, posicion = 6;
+        int cap = 5, cant = 5, posicion = 6;
         for (int i = 0; i < cant; i++)
         {
             Console.Write("{0}. ", i + 1);
@@ -15,11 +15,11 @@ public class E4_1_4_4
         }
         while (posicion > 5)
         {
-            Console.Write("Selecciona la posicion que quieras borrar.");
+            Console.Write("Selecciona la posicion que quieras borrar: ");
             posicion = Convert.ToInt32(Console.ReadLine());
             if((posicion > 5) || (posicion <= 0))
             {
-                Console.WriteLine("No existen posiciones superiores a 5.");
+                Console.WriteLine("posicion erronea.");
             }
         }
         for (int i = posicion - 1; i < cant - 1; i++)
@@ -30,5 +30,7 @@ public class E4_1_4_4
             Console.Write("{0}. ", i + 1);
             Console.WriteLine(numeros[i]);
         }
+        if (cap > cant)
+            Console.WriteLine("Queda {0} huecos con valor 0.", cap - cant);
     }
 }
