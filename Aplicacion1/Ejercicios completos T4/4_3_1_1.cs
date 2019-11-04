3cde6316d3fbb28d6d43e59af374e114a4ea8a1a
Juanjo <JRafet20@gmail.com>
2019-11-04 12:34:05 +0100

    4.3.1.1 Completado

---
+++ Aplicacion1/Ejercicios completos T4/4_3_1_1.cs
@@ -0,0 +1,32 @@
+﻿/*Ejercicio 4.3.2.1: Crea un "struct" que almacene datos de una canción en formato MP3:
+Artista, Título, Duración (en segundos), Tamaño del fichero (en KB). Un programa
+debe pedir los datos de una canción al usuario, almacenarlos en dicho "struct" y
+después mostrarlos en pantalla.*/
+using System;
+public class E4_3_1_1
+{
+    struct eseTemazo
+    {
+        public string artista, titulo;
+        public int durasegundos;
+        public double pesoKB;
+    }
+    public static void Ejecutar()
+    {
+        eseTemazo[] tema = new eseTemazo[1];
+
+        Console.Write("¿Artista?: ");
+        tema[0].artista = Convert.ToString(Console.ReadLine());
+        Console.Write("¿Título del tema?: ");
+        tema[0].titulo = Convert.ToString(Console.ReadLine());
+        Console.Write("¿Cuántos segundos dura?: ");
+        tema[0].durasegundos = Convert.ToInt32(Console.ReadLine());
+        Console.Write("¿Cuantos KB ocupa?: ");
+        tema[0].pesoKB = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Datos de {0}.mp3:", tema[0].titulo);
+        Console.WriteLine("Artista: {0}", tema[0].artista);
+        Console.WriteLine("Título: {0}", tema[0].titulo);
+        Console.WriteLine("Duracion(Secs): {0}", tema[0].durasegundos);
+        Console.WriteLine("Tamaño(KB): {0}", tema[0].pesoKB);
+    }
+}
--- Aplicacion1/Program.cs
+++ Aplicacion1/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(String[] args)
     {
-        E4_2_3.Ejecutar();
+        E4_3_1_1.Ejecutar();
         Console.ReadLine();
     }
 }
