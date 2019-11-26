98cd8b76663fd15cc35e6040b6b8b179a51fbdee
Juanjo <JRafet20@gmail.com>
2019-11-26 10:53:57 +0100

    4.4.3.2 completado.

---
+++ Aplicacion1/Ejercicios completos T4/4_4_3_2.cs
@@ -0,0 +1,16 @@
+﻿/*Ejercicio 4.4.3.2:  Un programa que pida una frase al usuario y la muestra en orden inverso
+(de la última letra a la primera).*/
+using System;
+public class E4_4_3_2
+{
+    public static void Ejecutar()
+    {
+        string frase;
+        Console.Write("¿Frase?: ");
+        frase = Convert.ToString(Console.ReadLine());
+        for(int i = frase.Length - 1; i >= 0; i--)
+        {
+            Console.Write(frase[i]);
+        }
+    }
+}
