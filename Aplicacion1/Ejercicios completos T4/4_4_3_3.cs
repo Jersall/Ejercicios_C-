db611b97ef4fb2cf130d2f5e0d2b5044e726a8a2
Juanjo <JRafet20@gmail.com>
2019-11-26 12:54:23 +0100

    4.4.3.3 completado.

---
+++ Aplicacion1/Ejercicios completos T4/4_4_3_3.cs
@@ -0,0 +1,32 @@
+﻿/*Ejercicio 4.4.3.3: Un programa que pida al usuario una frase, después una letra y
+finalmente diga si aparece esa letra como parte de esa frase o no.*/
+using System;
+public class E4_4_3_3
+{
+    public static void Ejecutar()
+    {
+        string frase;
+        char letra;
+        int contador = 0, largo;
+        Console.Write("Introduce una frase: ");
+        frase = Convert.ToString(Console.ReadLine());
+        largo = frase.Length;
+        Console.Write("Introduce una letra: ");
+        letra = Convert.ToChar(Console.ReadLine());
+        for(int i = 0; i < largo; i++)
+        {
+            if(frase[i] == letra)
+            {
+                contador++;
+            }
+        }
+        if (contador > 0)
+        {
+            Console.WriteLine("Hay " + contador + " letras " + letra + " en esta frase.");
+        }
+        else
+        {
+            Console.WriteLine("No hay letras " + letra + " en esta frase.");
+        }
+    }
+}
