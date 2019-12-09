7c59520e552cdb6ec067d99223f7655dfcbe0cc1
Juanjo <JRafet20@gmail.com>
2019-12-09 09:59:38 +0100

    4.4.4.2 completado

---
+++ Aplicacion1/Ejercicios completos T4/4_4_4_2.cs
@@ -0,0 +1,28 @@
+﻿/*Ejercicio 4.4.4.2: Un programa que te pida tu nombre y lo muestre en pantalla como un
+triángulo creciente.*/
+using System;
+public class E4_4_4_2
+{
+    public static void Ejecutar()
+    {
+        string nombre, subnombre;
+        Console.Write("¿Nombre?: ");
+        nombre = Convert.ToString(Console.ReadLine());
+        int numerolargo = nombre.Length;
+        for(int i = 0; i < numerolargo; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                subnombre = nombre.Substring(j, 1);
+                if(j == i)
+                {
+                    Console.WriteLine(subnombre);
+                }
+                else
+                {
+                    Console.Write(subnombre);
+                }
+            }
+        }
+    }
+}
--- Aplicacion1/Program.cs
+++ Aplicacion1/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(String[] args)
     {
-        E4_4_4_1.Ejecutar();
+        E4_4_4_2.Ejecutar();
         Console.ReadLine();
     }
 }
